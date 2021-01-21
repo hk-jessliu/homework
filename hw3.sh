95a3525a088a16b0aa91489962cda0d11e6afea5
ff9783aa <ff9783aa@gmail.com>
2021-01-21 10:51:20 +0800

    edit hw3.sh

--- hw3.sh
+++ hw3.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-while [ 1 ]
+while (true)
 do
 
-date1=$(date +"%Y%m%d")
-echo "$date1"
-sudo mkdir -p /tmp/$date1
-sleep 3600
+cd /tmp
+for filename  in 2021*
+do
+
+
+    #echo "${filename%.*}"   
+        nowtime=$(date +%Y%m%d%H%M)
+    #echo "${time1}"
+
+if [  "${filename%.*}" -le "${nowtime}"    ];then
+
+	date1=$(date +"%Y%m%d%H%M")
+    #echo "$date1"
+	sudo mkdir -p /tmp/$date1
+	echo "${date1} create file"
+    #sleep 5
+
+fi
+
 
 
 done
+done
+
