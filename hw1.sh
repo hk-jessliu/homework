bb08c3c6ca19907d6c4193b6e5681fa861bffad0
ff9783aa <ff9783aa@gmail.com>
2021-01-21 16:38:34 +0800

    add new description

--- hw1.sh
+++ hw1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#請試著寫出一個shell script名為userinfo.sh，使用-d參數，可以計算wayne12這個使用者在系統中共建立了幾個目錄？
+
+
+
 user=/home/jess
 
 if [ -d "$user" ];then
--- hw2.sh
+++ hw2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#請加上一個-p參數，計算目前有多少程序屬於wayne12這個使用者在使用？
+
+
 #process=`ps aux | grep jess`
 ps u | grep jess > ps.txt
 wc -l ps.txt
