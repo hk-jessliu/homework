f22c3302e9308f1ac12d2856ccabac93d9ee5ffa
ff9783aa <ff9783aa@gmail.com>
2021-01-21 10:54:01 +0800

    review code hw3.sh

--- hw3.sh
+++ hw3.sh
@@ -2,27 +2,20 @@
 
 while (true)
 do
-
+	
 cd /tmp
 for filename  in 2021*
 do
 
-
-    #echo "${filename%.*}"   
         nowtime=$(date +%Y%m%d%H%M)
-    #echo "${time1}"
 
 if [  "${filename%.*}" -le "${nowtime}"    ];then
 
 	date1=$(date +"%Y%m%d%H%M")
-    #echo "$date1"
 	sudo mkdir -p /tmp/$date1
 	echo "${date1} create file"
-    #sleep 5
 
 fi
-
-
 
 done
 done
